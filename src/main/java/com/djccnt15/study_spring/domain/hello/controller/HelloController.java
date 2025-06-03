b8bac73b45eadabaae9859d33758b73c7889b75b
Jay.D <djccnt15@gmail.com>
2025-06-03 17:21:03 +0900

    create basic rest controller

--- src/main/java/com/djccnt15/study_spring/domain/hello/controller/HelloController.java
+++ src/main/java/com/djccnt15/study_spring/domain/hello/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.djccnt15.study_spring.domain.hello.controller;
 
+import com.djccnt15.study_spring.domain.hello.model.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +23,17 @@ public class HelloController {
     ) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello %s".formatted(name);
+    }
+    
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        return Hello.builder().name(name).build();
     }
 }
---
+++ src/main/java/com/djccnt15/study_spring/domain/hello/model/Hello.java
@@ -0,0 +1,11 @@
+package com.djccnt15.study_spring.domain.hello.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Hello {
+
+    private String name;
+}
