d7386cc4ae7d8e25afbbfd8ed49c16ee9a2fa8aa
Jay.D <djccnt15@gmail.com>
2025-06-06 21:21:06 +0900

    create controller for member domain

---
+++ src/main/java/com/djccnt15/study_spring/domain/member/controller/MemberControllerBean.java
@@ -0,0 +1,40 @@
+package com.djccnt15.study_spring.domain.member.controller;
+
+import com.djccnt15.study_spring.db.model.MemberEntity;
+import com.djccnt15.study_spring.domain.member.model.MemberForm;
+import com.djccnt15.study_spring.domain.member.service.MemberServiceBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberControllerBean {
+    
+    private final MemberServiceBean memberServiceBean;
+    
+    @Autowired
+    public MemberControllerBean(MemberServiceBean memberServiceBean) {
+        this.memberServiceBean = memberServiceBean;
+    }
+    
+    @GetMapping("/members/new")
+    public String createMemberForm() {
+        return "members/member-create-form";
+    }
+    
+    @PostMapping("/members/new")
+    public String createMember(MemberForm form) {
+        var member = MemberEntity.builder().name(form.getName()).build();
+        memberServiceBean.join(member);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        var members = memberServiceBean.findMembers();
+        model.addAttribute("members", members);
+        return "members/member-list";
+    }
+}
---
+++ src/main/java/com/djccnt15/study_spring/domain/member/model/MemberForm.java
@@ -0,0 +1,11 @@
+package com.djccnt15.study_spring.domain.member.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MemberForm {
+
+    private String name;
+}
