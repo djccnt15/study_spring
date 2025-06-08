8410318fc31403d020f204655c525c54f60244e9
Jay.D <djccnt15@gmail.com>
2025-06-08 11:36:46 +0900

    create integrated test or service and repository

---
+++ src/test/java/com/djccnt15/study_spring/domain/member/service/MemberServiceIntegratedTest.java
@@ -0,0 +1,57 @@
+package com.djccnt15.study_spring.domain.member.service;
+
+import com.djccnt15.study_spring.db.model.MemberEntity;
+import com.djccnt15.study_spring.db.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional  // rollback db after each test
+class MemberServiceIntegratedTest {
+    
+    @Autowired MemberServiceBean memberServiceBean;
+    @Autowired MemberRepository memberRepositoryBean;
+    
+    @Test
+    void join() {
+        // given
+        var member = MemberEntity.builder().name("test").build();
+        
+        // when
+        var savedId = memberServiceBean.join(member);
+        
+        // then
+        var findMember = memberServiceBean.findOne(savedId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    
+    @Test
+    void dupedMemberException() {
+        // given
+        var member1 = MemberEntity.builder().name("test").build();
+        var member2 = MemberEntity.builder().name("test").build();
+        
+        // when
+        memberServiceBean.join(member1);
+        
+        // then
+        var e = assertThrows(
+            IllegalStateException.class,
+            () -> memberServiceBean.join(member2)
+        );
+        assertThat(e.getMessage()).isEqualTo("already exist member name");
+    }
+    
+    @Test
+    void findMembers() {
+    }
+    
+    @Test
+    void findOne() {
+    }
+}
