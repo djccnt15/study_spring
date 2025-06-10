01ad2d17e7c624aa94cd939673cc97a9c1a544d5
Jay.D <djccnt15@gmail.com>
2025-06-10 21:54:50 +0900

    create member page

--- src/main/java/com/djccnt15/study_spring/domain/member/controller/MemberController.java
+++ src/main/java/com/djccnt15/study_spring/domain/member/controller/MemberController.java
@@ -3,8 +3,11 @@ package com.djccnt15.study_spring.domain.member.controller;
 import com.djccnt15.study_spring.domain.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("member")
 public class MemberController {
     
     private final MemberService memberService;
@@ -12,5 +15,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    
+    @GetMapping
+    public String memberHome() {
+        return "member";
     }
 }
